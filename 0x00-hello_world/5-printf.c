cae703ed430a10b05dfc0d95c186cae4f8b9f1cb
AdrianChris <chrismadegwa4@gmail.com>
2023-03-16 15:48:12 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*function that prints out with puts
+*
+*Return: Always 0 (success)
+*
+*returns success
+*/
+int main(void)
+{
+	printf("\"with proper grammer,but the outcome is a piece of art,");
+	return (0); }
