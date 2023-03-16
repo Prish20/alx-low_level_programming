9b4d2503a34366ca02a74f4aa7b2279393d4c6ab
AdrianChris <chrismadegwa4@gmail.com>
2023-03-16 14:41:39 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,6 @@
 *
 *Return :Always 0 (success)
 */
-int main(void)
-/**
-*main - Point
-*/
+int main(void
 {puts("\"Programming is like building a multilingual puzzle");
-return (0); }
+return (0); } 
