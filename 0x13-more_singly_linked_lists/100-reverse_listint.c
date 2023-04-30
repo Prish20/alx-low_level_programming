abc3ec50bc07c8bef7cc13d4249696b567a07d43
AdrianChris <chrismadegwa4@gmail.com>
2023-04-29 17:49:58 -0700

    Reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,27 +9,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *current = *head, *next_ptr = NULL, *prev_ptr = NULL;
+	listint_t *current = *head, *next_ptr = NULL, *prev_ptr = NULL;
 
-    /* Traverse the list */
-    while (current != NULL)
-    {
-        /* Store the next node */
-        next_ptr = current->next;
+	while (current != NULL)
+	{
+		next_ptr = current->next;
+		current->next = prev_ptr;
+		prev_ptr = current;
+		current = next_ptr;
+	}
 
-        /* Change the next node to the previous node */
-        current->next = prev_ptr;
-
-        /* Move the previous node one position forward */
-        prev_ptr = current;
-
-        /* Move the current node one position forward */
-        current = next_ptr;
-    }
-
-    /* Update the pointer to the first node */
-    *head = prev_ptr;
-
-    return (*head);
+	*head = prev_ptr;
+	return (*head);
 }
 
