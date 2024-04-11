20d5aab4486478d3adc0098ca3e7e8b58693b38e
Prish20 <prish@students.uonbi.ac.ke>
2024-04-11 04:50:57 +0300

    11. Advanced binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,6 +12,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
-
+int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* MAIN_H */
